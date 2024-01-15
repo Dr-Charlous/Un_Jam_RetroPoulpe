93cc24841b8082f2412f145365fd0d36431bf487
1Coquo <1coquo@gmail.com>
2024-01-15 22:51:04 +0100

    Add speeddometer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public static int scoreValue = 0;
+    public  static float speedValue = 0;
     public static float time = 0;
     private int lastTime = 0;
     private int currentTime = 0;
 
-
-    void Start()
+    private void Start()
     {
-        scoreValue = 10;
+        StartCoroutine(CalcSpeed());
     }
 
     void Update()
     {
         time += Time.deltaTime;
+        currentTime = Mathf.FloorToInt(time);
 
-        if (lastTime != currentTime)
+        if (lastTime != currentTime) // A chaque seconde
         {
             scoreValue += 10;
         }
         lastTime = currentTime;
-        scoreValue += 10;
+
+    }
+
+    IEnumerator CalcSpeed()
+    {
+        while (true)
+        {
+            Vector3 prevPos = transform.position;
+
+            yield return new WaitForFixedUpdate();
+
+            speedValue = Mathf.RoundToInt(Vector3.Distance(transform.position, prevPos) / Time.fixedDeltaTime);
+        }
     }
 }
