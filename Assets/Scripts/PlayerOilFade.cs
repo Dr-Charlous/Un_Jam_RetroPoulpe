95bb005f8f79cbb525b9fc55087afd34d4bbee6a
albarold <74775300+albarold@users.noreply.github.com>
2024-01-16 16:22:15 +0100

    changement de couleurs

---
+++ Assets/Scripts/PlayerOilFade.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerOilFade : MonoBehaviour
+{
+    public Material PoulpeMaterial;
+    private Color OriginColor;
+    private bool IsBlack;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchOilState(0.5f);
+        }
+    }
+    private void Start()
+    {
+        OriginColor = PoulpeMaterial.GetColor("_BaseColor");
+    }
+    void SwitchOilState(float TransitionDuration)
+    {
+        if (!IsBlack)
+        {
+            DOVirtual.Color(OriginColor, Color.black, TransitionDuration, (value) =>
+            {
+                PoulpeMaterial.SetColor("_BaseColor", value);
+                Debug.Log(value);
+            });
+           
+            IsBlack = true;
+        }
+        else
+        {
+            DOVirtual.Color(Color.black,OriginColor, TransitionDuration, (value) =>
+            {
+                PoulpeMaterial.SetColor("_BaseColor", value);
+                Debug.Log(value);
+            });
+            IsBlack = false;
+        }
+        
+    }
+}
