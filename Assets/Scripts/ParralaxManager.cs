de42ee82825997dbccfaa86ce1e38f773eadb913
albarold <74775300+albarold@users.noreply.github.com>
2024-01-17 11:32:20 +0100

    gpes

--- Assets/Scripts/ParralaxManager.cs
+++ Assets/Scripts/ParralaxManager.cs
@@ -6,6 +6,7 @@ public class ParralaxManager : MonoBehaviour
 {
     public Material[] Materials;
     public GameObject[] Deco;
+    public GameObject[] Bonus;
     // Start is called before the first frame update
     public void UpdateParallax(float PlayerSpeed, float PlayerPosX)
     {
@@ -15,7 +16,7 @@ public class ParralaxManager : MonoBehaviour
         }
 
 
-        if (Random.Range(1, 100) == 2)
+        if (Random.Range(1, 1000) == 2)
         {
             float t = 2;
             GameObject go = GameObject.Instantiate(Deco[Random.Range(0, Deco.Length)], new Vector3(PlayerPosX + 25, 15, 2), Quaternion.identity);
@@ -25,6 +26,12 @@ public class ParralaxManager : MonoBehaviour
 
             }
             Destroy(go);*/
+        }
+        if (Random.Range(1, 500) == 2)
+        {
+            float t = 2;
+            GameObject go = GameObject.Instantiate(Bonus[Random.Range(0, Bonus.Length)], new Vector3(PlayerPosX + 25, Random.Range(5,10), 0), Quaternion.identity);
+            
         }
     }
 }
