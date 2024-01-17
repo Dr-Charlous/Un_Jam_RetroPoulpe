7c5ac4a7f007df60c4ede93bf1f8a6ce27439f9e
albarold <74775300+albarold@users.noreply.github.com>
2024-01-17 09:50:54 +0100

    update parallax

--- Assets/Scripts/ParralaxManager.cs
+++ Assets/Scripts/ParralaxManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class ParralaxManager : MonoBehaviour
 {
     public Material[] Materials;
+    public GameObject[] Deco;
     // Start is called before the first frame update
-    public void UpdateParallax(float PlayerSpeed)
+    public void UpdateParallax(float PlayerSpeed, float PlayerPosX)
     {
         foreach (Material mat in Materials)
         {
             mat.SetFloat("_PlayerSpeed", PlayerSpeed);
+        }
+
+        if (Random.Range(1,100)==2)
+        {
+            GameObject.Instantiate(Deco[Random.Range(0, Deco.Length)], new Vector3(PlayerPosX, 10, 0), Quaternion.identity);
         }
     }
 }
